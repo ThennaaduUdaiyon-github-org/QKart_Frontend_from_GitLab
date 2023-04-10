ee349dbb3d48e602524855de11dfb92bf7f7e5e2
CrioUser <crio-user@criodo.com>
2023-04-10 06:33:14 +0530

    ME_QKART_FRONTEND_V2_MODULE_LOGIN recommitted post failed tests due to INTERNET!

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,7 +12,6 @@ import React, { useEffect, useState } from "react";
 import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
-import ProductCard from "./ProductCard";
 import "./Products.css";
 
 const Products = () => {
@@ -22,14 +21,7 @@ const Products = () => {
         
       </Header>
 
-      {/* <ProductCard {
-"name":"Tan Leatherette Weekender Duffle",
-"category":"Fashion",
-"cost":150,
-"rating":4,
-"image":"https://crio-directus-assets.s3.ap-south-1.amazonaws.com/ff071a1c-1099-48f9-9b03-f858ccc53832.png",
-"_id":"PmInA797xJhMIPti"
-}/> */}
+      
 
       <Grid container>
         <Grid item className="product-grid">
